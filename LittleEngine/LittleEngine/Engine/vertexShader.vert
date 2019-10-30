75b4ea79abadd6e086d8483b7d885e82239611a2
yalania <anaisabel14hb@gmail.com>
2019-10-30 22:22:44 +0100

    Add color vertex

--- LittleEngine/LittleEngine/Engine/vertexShader.vert
+++ LittleEngine/LittleEngine/Engine/vertexShader.vert
@@ -1,5 +1,6 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 vertexColor;
 
 
 uniform mat4 view;
@@ -10,5 +11,5 @@ out vec4 color;
 void main()
 {
     gl_Position = proj*view*model*vec4(aPos, 1.0);
-	color = vec4(1.0,0.5,1.0,1.0);
+	color = vec4(vertexColor,1.0);
 }
--- LittleEngine/LittleEngine/VertexBufferObject.cpp
+++ LittleEngine/LittleEngine/VertexBufferObject.cpp
@@ -5,10 +5,11 @@
 VertexBufferObject::VertexBufferObject(){
 
 	float vertices[] = {
-		 0.5f, -0.5f, 0.0f, 
-	    0.5f,  0.2f, 0.0f,  
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.2f, 0.0f  };
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+	    0.5f,  0.2f, 0.0f,  0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		-0.5f,  0.2f, 0.0f, 0.0f, 0.0f, 0.0f,
+	};
 	unsigned int indices[] = {
 		0,1,2,
 		1,3,2
@@ -24,8 +25,12 @@ VertexBufferObject::VertexBufferObject(){
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// 3. then set our vertex attributes pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// 3. then set our vertex attributes pointers
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 }
 
 VertexBufferObject::~VertexBufferObject() {
