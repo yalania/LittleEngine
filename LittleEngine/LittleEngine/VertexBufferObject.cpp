5a556b52f879a83eb91ed710a3483c0be8f5af0d
yalania <anaisabel14hb@gmail.com>
2019-10-30 22:53:43 +0100

    Changing texture

--- LittleEngine/LittleEngine/VertexBufferObject.cpp
+++ LittleEngine/LittleEngine/VertexBufferObject.cpp
@@ -7,8 +7,8 @@
 VertexBufferObject::VertexBufferObject(){
 
 	float vertices[] = {
-		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	    0.5f,  0.2f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+	    0.5f,  0.2f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
 		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
 		-0.5f,  0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
@@ -57,17 +57,11 @@ void VertexBufferObject::Update(){
 void VertexBufferObject::LoadTexture() {
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 	// load image, create texture and generate mipmaps
 	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-	unsigned char *data = stbi_load("logo_UPC.jpg", &width, &height, &nrChannels, 0);
+
+	unsigned char *data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
