61a2a3b1e1abeb15db9222d752f1fb396cdc8c61
Anabel <48101863+yalania@users.noreply.github.com>
2019-11-06 19:28:22 +0100

    Component methods

--- LittleEngine/LittleEngine/Components/Component.h
+++ LittleEngine/LittleEngine/Components/Component.h
@@ -1,5 +1,24 @@
 #ifndef _COMPONENT_
 #define _COMPONENT_
+#include "../Globals.h"
+class Component {
 
+	Component() = default;
+	virtual ~Component() = default;
+
+	virtual bool Init()
+	{
+		return true;
+	}
+
+	virtual update_status Update() {
+		return UPDATE_CONTINUE;
+	};
+
+	virtual bool CleanUp()
+	{
+		return true;
+	}
+};
 #endif // !_COMPONENT_
 
