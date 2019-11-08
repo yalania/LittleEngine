19396bfef8b93b0e2b442dc088dee5063280182b
yalania <anaisabel14hb@gmail.com>
2019-11-08 20:06:08 +0100

    Add Mesh component

---
+++ LittleEngine/LittleEngine/Components/Mesh.h
@@ -0,0 +1,47 @@
+#ifndef _MESH_COMPONENT_
+#define _MESH_COMPONENT_
+#include "Component.h"
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+
+struct Vertex {
+	// position
+	glm::vec3 Position;
+	// normal
+	glm::vec3 Normal;
+	// texCoords
+	glm::vec2 TexCoords;
+	// tangent
+	glm::vec3 Tangent;
+	// bitangent
+	glm::vec3 Bitangent;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+class Mesh : public Component {
+
+public:
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	update_status Update() override;
+
+private:
+	void setupMesh();
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	
+	unsigned int VBO = 0;
+	unsigned int EBO = 0;
+	unsigned int VAO = 0;
+
+ };
+
+#endif // !_MESH_COMPONENT_
+
